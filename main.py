b4edbf9f8455695be0c2bd9ca98e72d0dc8716b0
HumanBones <humanbones@live.com>
2021-09-13 02:04:51 +0200

    added custom commands

--- main.py
+++ main.py
@@ -1,23 +1,54 @@
 import discord
 import settings
-import os
-import discord.ext
-from discord.ext import commands, tasks
-from discord.ext.commands.core import command
+import logging
+import random
+from discord.ext import commands, tasks 
 
 
 TOKEN = settings.TOKEN
-client = commands.Bot(command_prefix="!")
 
-class BotClient(discord.Client):
-    
-    @client.event
+def init_logger():
+    logger = logging.getLogger("discord")
+    logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+    handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
+    logger.addHandler(handler)
+
+
+
+class BotCommands(commands.Cog):
+
     async def on_ready(self):
         print("bot online")
 
-    @client.command
+
+    @commands.command()
     async def ping(self, ctx):
         await ctx.send("pong!")
 
-client = BotClient()
-client.run(TOKEN)
+    @commands.command()
+    async def test(self, ctx, args):
+        await ctx.send("yes "+ args)
+
+    @commands.command()
+    async def word(self, ctx):
+        self.words = ["cat","dog","human","tree","house","rock","water"]
+        random.shuffle(self.words)
+        self.word = random.choice(self.words)
+        await ctx.send("Your word is: "+ self.word)
+
+
+bot = commands.Bot(command_prefix="!")
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as:\n{0.user.name}\n{0.user.id}'.format(bot))
+
+#@bot.event
+#async def on_message(message):
+#    print("Message from {0.author}: {0.content}".format(message))
+
+bot.add_cog(BotCommands(bot))
+init_logger()
+bot.run(TOKEN)
